bbf5430ca13fd1a2ad214f99d42e7a3933b4b01a
suvrick <suvrick@outlook.com>
2021-03-16 20:49:38 +0700

    edit cors

--- middlewares/corsmiddleware.go
+++ middlewares/corsmiddleware.go
@@ -1,14 +1,23 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 // CORSMiddleware ...
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		w := c.Writer
+		r := c.Request
+		fmt.Println("origin >>> ", r.Header.Get("Origin"))
+		// js-ajax handling cross-domain issues
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT")
+		w.Header().Add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Cookie")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
@@ -17,4 +26,32 @@ func CORSMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
+	// return cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:4200"},
+	// 	AllowMethods:     []string{"PUT", "PATCH", "GET"},
+	// 	AllowHeaders:     []string{"Content-Type"},
+	// 	ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+	// 	AllowCredentials: true,
+	// })
+
+	// return func(c *gin.Context) {
+
+	// 	c.Header("Access-Control-Allow-Origin", "http://localhost:4200")
+	// 	c.Header("Access-Control-Allow-Methods", "*")
+	// 	c.Header("Access-Control-Allow-Headers", "*")
+	// 	c.Header("Content-Type", "application/json")
+	// 	// c.Writer.Header().Add("Access-Control-Allow-Origin", "origin")
+	// 	// //c.Writer.Header().Add("Access-Control-Allow-Origin", "https://suvricksoft.ru")
+	// 	// //c.Writer.Header().Add("Access-Control-Allow-Origin", "http://localhost:4200")
+	// 	// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// 	// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Cookie")
+	// 	// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	// 	if c.Request.Method == "OPTIONS" {
+	// 		c.AbortWithStatus(204)
+	// 		return
+	// 	}
+
+	// 	c.Next()
+	// }
 }
